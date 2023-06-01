2c8143c690c23c8d0aad93a2c7ec1d20d6d6edf7
João Rheinheimer <jprheinheimer50@gmail.com>
2023-06-01 10:36:24 -0300

    Just pushing to github

--- tools/bash/update-tool-links.sh
+++ tools/bash/update-tool-links.sh
@@ -3,7 +3,7 @@
 # This is to be run from the root directory and will create symlinks to
 # all the tools under `tools/py` in `bin`, with the `i-pi-` prefix.
 
-for tool in `ls tools/py`
+for tool in $(ls tools/py)
 do
     ln -s ../tools/py/$tool bin/i-pi-${tool%.*}
 done
